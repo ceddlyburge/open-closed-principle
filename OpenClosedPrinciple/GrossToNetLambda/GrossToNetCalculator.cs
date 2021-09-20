e8f4cfa35838335fe499dd160b0e31fc2c4733fe
CeddlyBurge <ceddlyburge@gmail.com>
2021-09-20 08:31:50 +0100

    Add lambda example

---
+++ OpenClosedPrinciple/GrossToNetLambda/GrossToNetCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenClosedPrinciple.GrossToNetLambda
+{
+    public class GrossToNetCalculator
+    {
+        public GrossToNetCalculator(
+            IReadOnlyList<double> dependentLosses, 
+            IReadOnlyList<double> independentLosses,
+            Func<double, double, double> grossToNetCalculator
+            )
+        {
+            double dependentLoss = 
+                CombinePercentages(dependentLosses.ToArray());
+
+            double independentLoss =
+                CombinePercentages(independentLosses.ToArray());
+
+            GrossToNet = grossToNetCalculator(dependentLoss, independentLoss);
+        }
+
+        double CombinePercentages(params double[] percentages)
+        {
+            double combination = 1;
+            foreach (var percentage in percentages)
+                combination *= 1 - percentage;
+            return 1 - combination;
+        }
+
+        public double GrossToNet { get; private set; }
+    }
+}
---
+++ OpenClosedPrinciple/GrossToNetLambda/TradeOffs.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenClosedPrinciple.GrossToNetLambda
+{
+    public class TradeOffs
+    {
+        // In this solution the code has become a lot more open to extension, but the trade off
+        // is that the calling code now has to know which losses are dependent and which are 
+        // independent, and how to calculate GrossToNet. This change is unlikely and the loss
+        // of encapsulation is significant, so this is probably not a good idea.
+        public TradeOffs(
+            IGrossEnergyYield grossYield,
+            double grossEnergy,
+            double hysteresisLoss,
+            double curtailmentLossGrid,
+            double turbineLossTurbulence,
+            double electricalLoss,
+            double turbineLossShear,
+            double turbinePerformanceExperience,
+            double operationalExperienceLoss)
+        {
+            var dependentLosses = new List<double> {
+                grossYield.TurbineAvailability,
+                grossYield.BalanceAvailability,
+                grossYield.AccessibilityAvailability,
+                hysteresisLoss,
+                electricalLoss,
+                grossYield.EnvironmentalShutdownWeather,
+                grossYield.EnvironmentalSiteAccess,
+                grossYield.EnvironmentTreeGrowth
+            };
+
+            var independentLosses = new List<double> {
+                grossYield.GridAvailability,
+                turbinePerformanceExperience,
+                turbineLossTurbulence,
+                grossYield.EnvironmentalPerformanceDegradationIcing,
+                grossYield.CurtailmentPowerPurchase,
+                grossYield.SubOptimalPerformance,
+                turbineLossShear,
+                operationalExperienceLoss
+            };
+
+
+            Func<double, double, double> grossToNetCalculator =
+                (double dependentLoss, double independentLoss) =>
+                           1 - (1 - (dependentLoss + curtailmentLossGrid)) * (1 - independentLoss);
+
+            var grossToNet = 
+                new GrossToNetCalculator(dependentLosses, independentLosses, grossToNetCalculator)
+                .GrossToNet;
+        }
+    }
+}
